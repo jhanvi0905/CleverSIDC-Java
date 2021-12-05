c15fb55a6617134fe7fe660fdb6ef369a6b2d555
zalakpatel315 <zalakpatel315@gmail.com>
2021-12-05 01:41:29 -0500

    Generalized code using design pattern, added contain function and command support
    Generalized code using design pattern, added contain function and command support

--- src/DT.java
+++ src/DT.java
@@ -1,9 +1,10 @@
 public interface DT {
-    public void addElement(int ID, String value);
-    public void remove(int ID);
+    public void addElement(int studentID, String value);
+    public void remove(int studentID);
     public void allKeys();
-    public int nextKey(int ID);
-    public int prevKey(int ID);
+    public int nextKey(int studentID);
+    public int prevKey(int studentID);
     public int rangeKey(int k1, int k2);
     public String getValues(int k1);
+    public boolean contains(int studentID);
 }
