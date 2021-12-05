af709ce8acd6a0e54ea24278042f36f8ff5de6cb
zalakpatel315 <zalakpatel315@gmail.com>
2021-12-05 04:32:25 -0500

    Added java doc
    Added java doc

--- src/DT.java
+++ src/DT.java
@@ -1,10 +1,63 @@
+/**
+ * Common interface consisting declaration of functions to be performed on
+ * system
+ *
+ */
 public interface DT {
-    public void addElement(int studentID, String value);
-    public void remove(int studentID);
-    public void allKeys();
-    public int nextKey(int studentID);
-    public int prevKey(int studentID);
-    public int rangeKey(int k1, int k2);
-    public String getValues(int k1);
-    public boolean contains(int studentID);
+	/**
+	 * Adds new Elements with student ID and student information
+	 * 
+	 * @param student ID
+	 * @param student information
+	 */
+	public void addElement(int studentID, String value);
+
+	/**
+	 * Removes element with student ID passed
+	 * 
+	 * @param studentID student ID
+	 */
+	public void remove(int studentID);
+
+	/**
+	 * Returns all elements present
+	 *
+	 */
+	public void allKeys();
+
+	/**
+	 * Finds successor student ID of given student ID
+	 * 
+	 * @return successor student ID
+	 */
+	public int nextKey(int studentID);
+
+	/**
+	 * Finds predecessor of the given student ID
+	 *
+	 * @return predecessor student ID
+	 */
+	public int prevKey(int studentID);
+
+	/**
+	 * Finds number of keys present between provided two keys
+	 *
+	 * @param key1 range start
+	 * @param key2 range end
+	 * @return number of nodes present
+	 */
+	public int rangeKey(int k1, int k2);
+
+	/**
+	 * Gets student information corresponding to student ID passed
+	 *
+	 * @param student ID
+	 * @return student information
+	 */
+	public String getValues(int k1);
+
+	/**
+	 * Checks if student with given student ID already exists in the system or not
+	 */
+	public boolean contains(int studentID);
 }
